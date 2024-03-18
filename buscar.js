71e5e7725ce607a941aac6fbd661688645c93d6e
C-Augusto-F <146351011+C-Augusto-F@users.noreply.github.com>
2024-03-18 18:33:38 -0300

    Add files via upload

---
+++ buscar.js
@@ -0,0 +1,25 @@
+const barraBusca = document.getElementById('barraBusca');
+const botaoBuscar = document.getElementById('botaoBuscar');
+const resultadosDiv = document.getElementById('resultados');
+
+botaoBuscar.addEventListener('click', () => {
+    const conteudo = barraBusca.value.trim();
+    
+    fetch(`http://127.0.0.1:8080/buscar?conteudo=${conteudo}`, {
+        mode: 'cors', // Habilita o modo CORS
+        headers: {
+            'Origin': 'http://3.131.153.88' // Define a origem da solicitação
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        resultadosDiv.innerHTML = '';
+        
+        data.resultados.forEach(resultado => {
+            resultadosDiv.innerHTML += `<p><strong>Título:</strong> ${resultado.titulo} | <strong>Link:</strong> <a href="${resultado.link}">${resultado.link}</a></p>`;
+        });
+    })
+    .catch(error => {
+        console.error('Erro ao buscar filmes:', error);
+    });
+});
